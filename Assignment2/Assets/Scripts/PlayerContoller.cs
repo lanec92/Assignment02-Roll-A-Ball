08e08e3be6ab533ec58dc6fd29cad18b33f15c05
Lane <lanec92@gmail.com>
2021-02-02 20:59:38 -0600

    Completed Tutorial Pre-Modifications

--- Assignment2/Assets/Scripts/PlayerContoller.cs
+++ Assignment2/Assets/Scripts/PlayerContoller.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerContoller : MonoBehaviour
 {
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
     private float movementX;
     private float movementY;
-    public float speed = 0;
+    private int count;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -20,12 +30,30 @@ public class PlayerContoller : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
-
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+
+            SetCountText();
+        }
     }
 }

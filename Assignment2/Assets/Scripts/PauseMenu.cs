e3b354e434bef1a71443e4f6fde805f226e0d885
Lane <lanec92@gmail.com>
2021-02-13 12:06:50 -0600

    Pre Turn In/ Final touches

--- Assignment2/Assets/Scripts/PauseMenu.cs
+++ Assignment2/Assets/Scripts/PauseMenu.cs
@@ -45,7 +45,7 @@ public class PauseMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+        //UnityEditor.EditorApplication.isPlaying = false;
         Debug.Log("Quitting game...");
         Application.Quit();
     }
